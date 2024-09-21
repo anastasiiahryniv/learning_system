6c8140e981276e2683898dc82292f8c79787ee16
ahryniv <ahryniv@cisco.com>
2024-09-21 19:49:40 +0200

    rubocop-rails

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,7 +1,7 @@
- require "test_helper"
+require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get landing_page" do
+  test 'should get landing_page' do
     get root_url
     assert_response :success
   end
