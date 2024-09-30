a2196ad6bb522696d63a1bb6eef2f5ceb729ce4f
ahryniv <ahryniv@cisco.com>
2024-09-30 23:53:32 +0200

    rubocop fix

--- app/models/course.rb
+++ app/models/course.rb
@@ -1,6 +1,6 @@
 class Course < ApplicationRecord
   belongs_to :instructor, class_name: 'Instructor'
-  has_many :enrollments
+  has_many :enrollments, dependent: :destroy
   has_many :students, through: :enrollments
 
   validates :name, presence: true
--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -1,3 +1,3 @@
 class Instructor < User
-  has_many :courses
+  has_many :courses, dependent: :destroy
 end
