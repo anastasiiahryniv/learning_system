abaf9b79bc7d84d2184b24563420021ed2c963b2
ahryniv <ahryniv@cisco.com>
2024-12-13 12:53:46 +0100

    Added more testes

---
+++ spec/controllers/students/profiles_controller_spec.rb
@@ -0,0 +1,39 @@
+require 'rails_helper'
+
+RSpec.describe Students::ProfilesController, type: :controller do
+  let(:student) { create(:student) }
+  let(:course) { create(:course) }
+  let(:enrollment) { create(:enrollment, student: student, course: course) }
+
+  before { sign_in student }
+
+  describe '#show' do
+    it 'shows the student profile with enrollments' do
+      get :show
+      expect(assigns(:student)).to eq(student.decorate)
+      expect(assigns(:enrollments)).to include(enrollment)
+    end
+
+    it 'renders the edit template' do
+      get :edit
+      expect(response).to render_template(:edit)
+    end
+  end
+
+  describe '#update' do
+    it 'updates the student profile with valid attributes' do
+      patch :update, params: { student: { name: 'New Name', surname: 'New Surname' } }
+      student.reload
+      expect(student.name).to eq('New Name')
+      expect(student.surname).to eq('New Surname')
+      expect(response).to redirect_to(students_profiles_path)
+      expect(flash[:notice]).to eq('Profile was successfully updated.')
+    end
+
+    it 'does not update the student profile with invalid attributes' do
+      patch :update, params: { student: { name: '', surname: '' } }
+      expect(response).to render_template(:edit)
+      expect(flash[:alert]).to eq("Name can't be blank, Surname can't be blank")
+    end
+  end
+end
---
+++ spec/queries/courses_query_spec.rb
@@ -0,0 +1,48 @@
+require 'rails_helper'
+
+RSpec.describe CoursesQuery do
+  let(:student) { create(:student) }
+  let(:instructor) { create(:instructor) }
+  let(:course1) { create(:course, name: 'B Course', tags: [tag]) }
+  let(:course2) { create(:course, name: 'A Course') }
+  let(:enrollment) { create(:enrollment, student: student, course: course1) }
+  let(:tag) { create :tag, name: 'Ruby' }
+
+  describe 'sorting' do
+    it 'returns courses sorted by name ascending' do
+      query = CoursesQuery.new(relation: Course.all, params: { sort_by: 'name_asc' }, student: nil)
+      result = query.call
+      expect(result).to eq([course2, course1])
+    end
+
+    it 'returns courses sorted by name descending' do
+      query = CoursesQuery.new(relation: Course.all, params: { sort_by: 'name_desc' }, student: nil)
+      result = query.call
+      expect(result).to eq([course1, course2])
+    end
+
+    it 'filters courses by tag' do
+      query = CoursesQuery.new(relation: Course.all, params: { tag: 'Ruby' }, student: nil)
+      result = query.call
+      expect(result).to eq([course1])
+    end
+
+    it 'excludes courses the student is enrolled in' do
+      query = CoursesQuery.new(relation: Course.all, params: {}, student: student)
+      result = query.call
+      expect(result).to eq([course2])
+    end
+
+    it 'returns all courses if no search parameters are provided' do
+      query = CoursesQuery.new(relation: Course.all, params: {}, student: nil)
+      result = query.call
+      expect(result).to eq([course1, course2])
+    end
+
+    it 'returns courses matching the search query' do
+      query = CoursesQuery.new(relation: Course.all, params: { search: { name_cont: 'A' } }, student: nil)
+      result = query.call
+      expect(result).to eq([course2])
+    end
+  end
+end
