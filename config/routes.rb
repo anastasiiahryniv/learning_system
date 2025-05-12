956952a0cf6736ef017e1251ed85660ab8c7aacc
ahryniv <ahryniv@cisco.com>
2025-05-13 00:28:29 +0200

    Add internationalisation to project

--- config/routes.rb
+++ config/routes.rb
@@ -21,18 +21,19 @@ Rails.application.routes.draw do
     mount Sidekiq::Web => '/sidekiq'
   end
 
-  resources :courses do
-    member do
-      patch :start
-    end
+  scope "(:locale)", locale: /en|es/ do
+    resources :courses do
+      member do
+        patch :start
+      end
 
-    resources :comments do
-      resources :replies, controller: 'comments'
-    end
+      resources :comments do
+        resources :replies, controller: 'comments'
+      end
 
-    resources :enrollments, only: [:create]
+      resources :enrollments, only: [:create]
+    end
   end
-
 
   resources :enrollments, only: [:index]
 
